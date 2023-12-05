992a0fde0c84b46233a36b70500b0c13de2c83d1
dimassamiaji <dimassamji@gmail.com>
2023-12-05 11:26:09 +0700

    update.js

---
+++ LEET CODE/codechallenge.js
@@ -0,0 +1,19 @@
+function memoize(fn) {
+    const map = new Map();
+    return function (...args) {
+      if (!map.has(args.join(","))) {
+        map.set(args.join(","), fn(...args));
+      }
+      return map.get(args.join(","));
+    };
+  }
+  
+  let callCount = 0;
+  const memoizedFn = memoize(function (a, b) {
+    callCount += 1;
+    return a + b;
+  });
+  memoizedFn(2, 3); // 5
+  memoizedFn(2, 3); // 5
+  console.log(callCount); // 1
+  
